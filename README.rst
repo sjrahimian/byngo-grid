42322a7d6a8016a6d54fb6fa9b1207c067722b05
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2024-01-22 23:32:14 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+Bango: Bingo Board Generator
+
+Generate random bingo boards between a minimum and maximum set of numbers.
+
+Options:
+* Maximum and minium values
+* grid size: 3x3, 4x4, or 5x5
+* Free space (default)
+* Banner
+* PDF export: 1, 2, or 4 grids/page
+
---
+++ bango.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+""" Bango: Bingo Board Generator
+
+    Random bingo board generator.
+"""
+
+
+import argparse
+import sys
+
+def args(): 
+    parser = argparse.ArgumentParser(prog='Bango', description='Random ningo board generator.', epilog='')
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+    
+sys.exit(0)
+
